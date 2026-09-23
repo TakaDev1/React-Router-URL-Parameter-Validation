c3eb2d342d5a632165bd9cf8c7a1e6aaec52fa55
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-23 20:43:52 +0900

    style: Project Adjustment Design

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,13 +4,38 @@ import { Link } from "react-router";
 
 const Navigation = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/product/1">商品 1</Link>
-        <Link to="/product/2">商品 2</Link>
-        <Link to="/product/3">商品 3</Link>
-        <Link to="/product/test">商品 4</Link>
-        <Link to="/product/5">商品 5</Link>
+    <div className="w-full">
+      <nav className="w-1/2 flex justify-around mx-auto py-10 gap-3">
+        <Link
+          to="/product/1"
+          className="w-1/5 bg-gray-500 text-white rounded-xl py-1 cursor-pointer hover:opacity-80 transition"
+        >
+          商品 1
+        </Link>
+        <Link
+          to="/product/2"
+          className="w-1/5 bg-gray-500 text-white rounded-xl py-1 cursor-pointer hover:opacity-80 transition"
+        >
+          商品 2
+        </Link>
+        <Link
+          to="/product/3"
+          className="w-1/5 bg-gray-500 text-white rounded-xl py-1 cursor-pointer hover:opacity-80 transition"
+        >
+          商品 3
+        </Link>
+        <Link
+          to="/product/test"
+          className="w-1/5 bg-gray-500 text-white rounded-xl py-1 cursor-pointer hover:opacity-80 transition"
+        >
+          商品 4
+        </Link>
+        <Link
+          to="/product/5"
+          className="w-1/5 bg-gray-500 text-white rounded-xl py-1 cursor-pointer hover:opacity-80 transition"
+        >
+          商品 5
+        </Link>
       </nav>
     </div>
   );
