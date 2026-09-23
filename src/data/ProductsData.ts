49f395309400217d69b73a41c0c6ecda5b7b51c1
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-23 18:53:11 +0900

    feat: Implement Products Data

---
+++ src/data/ProductsData.ts
@@ -0,0 +1,18 @@
+import type { Product } from "../types/ProductType";
+
+const ProdcutsData: Record<number, Product> = {
+  1: {
+    name: "PC",
+    price: 320000,
+  },
+  2: {
+    name: "SmartPhone",
+    price: 270000,
+  },
+  3: {
+    name: "Tablet",
+    price: 300000,
+  },
+};
+
+export default ProdcutsData;
